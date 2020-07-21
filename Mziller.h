07965dbb27d69556e5d0dbffde4daf263f856aff
LionelC-Kz <68594220+LionelC-Kz@users.noreply.github.com>
2020-07-21 20:23:57 +0800

    Initial

---
+++ Mziller.h
@@ -0,0 +1,292 @@
+﻿#pragma once
+#include < tlhelp32.h >
+#include < tchar.h >
+#include < iomanip >
+#include < string >
+#ifndef __AFXWIN_H__
+#include < Windows.h >
+#include < atlstr.h >
+#endif // !__AFXWIN_H__
+
+using namespace std;
+
+#define logbox(fmt, ...)\
+CString str; \
+str.Format(CString(fmt), __VA_ARGS__); \
+AfxMessageBox(str);
+
+#define endoffset -1
+#define endoffset64 (DWORD64)-1
+
+class Mziller
+{
+public:
+	HWND GetMousePointWindow()
+	{
+		POINT pos;
+		GetCursorPos(&pos);
+		return WindowFromPoint(pos);;
+	}
+	void WriteMemory(void* value, HANDLE processHANDLE, ...)
+	{
+		DWORD valueSize = sizeof(value);
+		if (value == NULL || valueSize == 0 || processHANDLE == NULL) return;
+		DWORD tempValue = 0;
+		va_list addresses;
+		va_start(addresses, valueSize);
+		DWORD offset = 0;
+		DWORD lastAddress = 0;
+		while ((offset = va_arg(addresses, DWORD)) != -1)
+		{
+			lastAddress = tempValue + offset;
+			::ReadProcessMemory(processHANDLE, (LPCVOID)lastAddress, &tempValue, sizeof(DWORD), 0);
+		}
+		va_end(addresses);
+
+		::WriteProcessMemory(processHANDLE, (LPVOID)lastAddress, value, valueSize, NULL);
+	}
+
+	void WriteMemory(void* value, HANDLE processHANDLE, DWORD address)
+	{
+		WriteMemory(value, processHANDLE, address, -1);
+	}
+
+	void ReadMemory(DWORD& value, HANDLE processHANDLE, ...)
+	{
+		DWORD valueSize = sizeof(value);
+		if (processHANDLE == NULL) return;
+
+		DWORD tempValue = 0;
+		va_list addresses;
+		va_start(addresses, valueSize);
+		DWORD offset = 0;
+		DWORD lastAddress = 0;
+		while ((offset = va_arg(addresses, DWORD)) != -1)
+		{
+			lastAddress = tempValue + offset;
+			::ReadProcessMemory(processHANDLE, (LPCVOID)lastAddress, &tempValue, sizeof(DWORD), 0);
+		}
+
+		value = tempValue;
+		va_end(addresses);
+	}
+
+	void ReadMemory(DWORD& value, HANDLE processHANDLE, DWORD address)
+	{
+		ReadMemory(value, processHANDLE, address, -1);
+	}
+
+	void GetMemoryAddress(DWORD& address, HANDLE processHANDLE, ...)
+	{
+		DWORD valueSize = sizeof(address);
+		if (processHANDLE == NULL) return;
+
+		DWORD tempValue = 0;
+		va_list addresses;
+		va_start(addresses, valueSize);
+		DWORD offset = 0;
+		DWORD lastAddress = 0;
+		while ((offset = va_arg(addresses, DWORD)) != -1)
+		{
+			lastAddress = tempValue + offset;
+			::ReadProcessMemory(processHANDLE, (LPCVOID)lastAddress, &tempValue, sizeof(DWORD), 0);
+		}
+
+		address = lastAddress;
+		va_end(addresses);
+	}
+
+	DWORD GetModuleBase(const wchar_t* ModuleName, DWORD ProcessId)
+	{
+		MODULEENTRY32 ModuleEntry = { 0 };
+		HANDLE SnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, ProcessId);
+
+		if (!SnapShot)
+			return NULL;
+
+		ModuleEntry.dwSize = sizeof(ModuleEntry);
+
+		if (!Module32First(SnapShot, &ModuleEntry))
+		{
+			return NULL;
+		}
+		do
+		{
+			if (!wcscmp(ModuleEntry.szModule, ModuleName))
+			{
+				CloseHandle(SnapShot);
+				return (DWORD)ModuleEntry.modBaseAddr;
+
+			}
+		}
+		while (Module32Next(SnapShot, &ModuleEntry));
+		CloseHandle(SnapShot);
+		return NULL;
+	}
+
+	void WriteMemory64(void* value, HANDLE processHANDLE, ...)
+	{
+		DWORD valueSize = sizeof(valueSize);
+		if (value == NULL || valueSize == 0 || processHANDLE == NULL) return;
+
+		DWORD64 tempValue = 0;
+		va_list addresses;
+		va_start(addresses, valueSize);
+		DWORD64 offset = 0;
+		DWORD64 lastAddress = 0;
+		while ((offset = va_arg(addresses, DWORD64)) != -1)
+		{
+			lastAddress = tempValue + offset;
+			::ReadProcessMemory(processHANDLE, (LPCVOID)lastAddress, &tempValue, sizeof(DWORD64), 0);
+		}
+		va_end(addresses);
+
+		::WriteProcessMemory(processHANDLE, (LPVOID)lastAddress, value, valueSize, NULL);
+	}
+
+	void WriteMemory64(void* value, HANDLE processHANDLE, DWORD64 address)
+	{
+		WriteMemory64(value, processHANDLE, address, -1);
+	}
+
+	void ReadMemory64(DWORD64& value, HANDLE processHANDLE, ...)
+	{
+		DWORD64 valueSize = sizeof(value);
+		if (processHANDLE == NULL) return;
+
+		DWORD64 tempValue = 0;
+		va_list addresses;
+		va_start(addresses, valueSize);
+		DWORD64 offset = 0;
+		DWORD64 lastAddress = 0;
+		while ((offset = va_arg(addresses, DWORD64)) != -1)
+		{
+			lastAddress = tempValue + offset;
+			::ReadProcessMemory(processHANDLE, (LPCVOID)lastAddress, &tempValue, sizeof(DWORD64), 0);
+		}
+		value = tempValue;
+		va_end(addresses);
+	}
+
+	void ReadMemory64(DWORD64& value, HANDLE processHANDLE, DWORD64 address)
+	{
+		ReadMemory64(value, processHANDLE, address, (DWORD64)-1);
+	}
+
+	void GetMemoryAddress64(DWORD64& address, HANDLE processHANDLE, ...)
+	{
+		DWORD64 valueSize = sizeof(address);
+		if (processHANDLE == NULL) return;
+
+		DWORD64 tempValue = 0;
+		va_list addresses;
+		va_start(addresses, valueSize);
+		DWORD64 offset = 0;
+		DWORD64 lastAddress = 0;
+		while ((offset = va_arg(addresses, DWORD64)) != -1)
+		{
+			lastAddress = tempValue + offset;
+			::ReadProcessMemory(processHANDLE, (LPCVOID)lastAddress, &tempValue, sizeof(DWORD64), 0);
+		}
+
+		address = lastAddress;
+		va_end(addresses);
+	}
+
+	DWORD64 GetModuleBase64(const wchar_t* ModuleName, DWORD ProcessId)
+	{
+		MODULEENTRY32 ModuleEntry = { 0 };
+		HANDLE SnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, ProcessId);
+
+		if (!SnapShot)
+			return NULL;
+
+		ModuleEntry.dwSize = sizeof(ModuleEntry);
+		if (!Module32First(SnapShot, &ModuleEntry))
+		{
+			return NULL;
+		}
+		do
+		{
+			if (!wcscmp(ModuleEntry.szModule, ModuleName))
+			{
+				CloseHandle(SnapShot);
+				return (DWORD64)ModuleEntry.modBaseAddr;
+
+			}
+		}
+		while (Module32Next(SnapShot, &ModuleEntry));
+		CloseHandle(SnapShot);
+		return NULL;
+	}
+	DWORD Cstr_dw(CString Cstr_x)
+	{
+		long long_x = _ttol(Cstr_x);  //CString to char*
+		DWORD DWORD_x = (DWORD)long_x; // char* to DWORD
+		return DWORD_x;
+	}
+	CString dw_Cstr(DWORD dw_x)
+	{
+		CString Cstr_x;
+		Cstr_x.Format(_T("%d"), dw_x);
+		return Cstr_x;
+	}
+	float dw_float(DWORD dw_x)
+	{
+		float flo_x = *reinterpret_cast<float*>(&dw_x);
+		return flo_x;
+	}
+	int Cstr_int(CString Cstr_x)
+	{
+		int int_x = _wtoi(Cstr_x);
+		return int_x;
+	}
+	double Cstr_dou(CString Cstr_x)
+	{
+		double dou_x = _wtof(Cstr_x);
+		return dou_x;
+	}
+
+	string Cstr_str(CString cstr)
+	{
+		string str(cstr.GetLength(), 'e');
+		for (int k = 0; k < cstr.GetLength(); k++)
+		{
+			str[k] = (char)cstr[k];
+		}
+		return str;
+	}
+	CString str_Cstr(string str)
+	{
+		CString cstr;
+		for (int k = 0; k < (int)str.length(); k++)
+		{
+			cstr += str[k];
+		}
+		return cstr;
+	}
+	string charp_str(char* charstar, int count)
+	{
+		string str(charstar, count);
+		return str;
+	}
+	char* charp_str(string& str)
+
+	{
+
+		int str_len = str.length();
+
+		char* charstar = new char[str_len + 1];
+
+		for (int k = 0; k < str_len; k++)
+
+		{
+
+			charstar[k] = str[k];
+
+		}
+
+		charstar[str_len] = '\0';
+		return charstar;
+	}
+};
--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# Win32MemoryHeader
- 
+# WinFunctionHeader
+
+You should include this header to your C++ Project by
+	#inclde "Mziller.h"
+	
+You should also create a object by
+	Mziller mz;
+or
+	Mziller* mz = new Mziller;
+
+For using the memory function you should also declare some globle variable by
+	HWND g_windowsHANDLE;
+	HANDLE g_processHANDLE;
+	DWORD g_pid;
+
+For finding the windows handle you can find it by the windows' name
+	g_windowsHANDLE = FindWindow(CString("Minesweeper"), CString("Minesweeper"));
+	
+Or find it by the mousepoint 
+	g_windowsHANDLE = mz.GetMousePointWindow()
+
+Find the ProcessID by
+	GetWindowThreadProcessId(g_windowsHANDLE, &g_pid);//
+	
+Find the Process Handle by
+	g_processHANDLE = OpenProcess(PROCESS_ALL_ACCESS, false, g_pid);
+	
+Function demo
+In 32bits enviroment:
+	DWORD value;
+	mz.WriteMemory(&value, g_processHANDLE, 0x75B424, 0x44, 0x68, 0x768, 0x1c, 0x555c, endoffset);
+	
+	BYTE set = {0x12, 0x1b}
+	mz.WriteMemory(&set, g_processHANDLE, 0x75B424);
+	
+	DWORD value;
+	mz.ReadMemory(value, g_processHANDLE, 0x75B424,0x44, 0x68, 0x768, 0x1c, 0x555c, endoffset);
+	mz.ReadMemory(value, g_processHANDLE, 0x75B424);
+	
+	DWORD address;
+	mz.GetMemoryAddress(value, g_processHANDLE, 0x75B424, 0x44, 0x68, 0x768, 0x1c, endoffset);
+	DWORD minesweeperbase = GetModuleBase(L"minesweeper.exe", g_pid);
+	
+In 64bits enviroment:
+	DWORD value;
+	mz.WriteMemory64(&value, g_processHANDLE, (DWORD64)0x75B424, (DWORD64)0x44, (DWORD64)0x68, (DWORD64)0x768, (DWORD64)0x1c, (DWORD64)0x555c, endoffset64);
+	
+	BYTE set = {0x12, 0x1b}
+	mz.WriteMemory64(&set, g_processHANDLE, (DWORD64)0x75B424);
+	
+	DWORD value;
+	mz.ReadMemory64(value, g_processHANDLE, (DWORD64)0x75B424,(DWORD64)0x44, (DWORD64)0x68, (DWORD64)0x768, (DWORD64)0x1c, (DWORD64)0x555c, endoffset64);
+	mz.ReadMemory64(value, g_processHANDLE, (DWORD64)0x75B424);
+	
+	DWORD address;
+	mz.GetMemoryAddress64(value, g_processHANDLE, (DWORD64)0x75B424, (DWORD64)0x44, (DWORD64)0x68, (DWORD64)0x768, (DWORD64)0x1c, endoffset64);
+	DWORD minesweeperbase = GetModuleBase64(L"minesweeper.exe", g_pid);
+	
+
